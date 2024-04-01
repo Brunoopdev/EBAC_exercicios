6d62de8959db5e928b23f06977877ceb25fdf941
Bruno <gpaes247@gmail.com>
2024-03-31 22:55:05 -0300

    commitando módulo AJAX

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const avatar = document.querySelector('.profile-avatar');
+    const nome = document.querySelector('.profile-name');
+    const username = document.querySelector('.profile-username');
+    const repositorios = document.querySelector('.repositories');
+    const seguidores = document.querySelector('.followers');
+    const seguindo = document.querySelector('.following');
+    const linkRepositorio = document.querySelector('.profile-link');
+
+
+    fetch('https://api.github.com/users/Brunoopdev')
+    .then(function(resposta){
+        return resposta.json();
+    })
+    .then(function(json){
+        avatar.src = json.avatar_url;
+        nome.innerHTML = `${json.name}`;
+        username.innerHTML = `@${json.login}`;
+        repositorios.innerHTML = `${json.public_repos}`;
+        seguidores.innerHTML = `${json.followers}`
+        seguindo.innerHTML = `${json.following}`
+        linkRepositorio.href = json.html_url;
+        console.log(json);
+    })
+})
