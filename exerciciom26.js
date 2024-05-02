5f2bd763c9c2a1e0e3a06d38a290f9ada57911cd
Bruno <gpaes247@gmail.com>
2024-05-01 21:05:10 -0300

    Add files via upload

---
+++ exerciciom26.js
@@ -0,0 +1,10 @@
+"use strict";
+const multiplica = (numeroA, numeroB) => {
+    return numeroA * numeroB;
+};
+multiplica(10, 20);
+const saudacao = (nome) => {
+    return 'Olá ' + nome;
+};
+
+saudacao('Bruno');
---
+++ exerciciom26.ts
@@ -0,0 +1,11 @@
+const multiplica = (numeroA:number, numeroB:number)  =>{
+    return numeroA * numeroB;
+}
+
+multiplica(10, 20);
+
+const saudacao = (nome:string) =>{
+    return 'Olá ' + nome;
+}
+
+saudacao('Bruno');
